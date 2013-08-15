b1339aba8b50e89176e9298c7102a5bffb03e188
quest <quest@mac.com>
2013-08-15 15:15:58 -0700

    More spec refactors

--- spec/starcraft2/profile/achievement_item_spec.rb
+++ spec/starcraft2/profile/achievement_item_spec.rb
@@ -4,18 +4,17 @@ describe Starcraft2::Profile::AchievementItem do
   describe '.initialize' do
     let(:achievement_item) { Starcraft2::Profile::AchievementItem.new(@options) }
 
-    it 'should store underscored attributes' do
-      @options = {:achievement_id => 1, :completion_date => 13333333}
+    it 'should store attributes as underscored' do
+      @options = {'achievementId' => 1, 'completionDate' => 13333333}
 
       achievement_item.achievement_id.should == 1
       achievement_item.completion_date.should == 13333333
     end
 
-    it 'should store camelCased attributes as underscored' do
-      @options = {'achievementId' => 1, 'completionDate' => 13333333}
+    it 'should use Stracraft2::Utils.load to populate the model' do
+      Starcraft2::Utils.should_receive(:load).with(anything, @options)
 
-      achievement_item.achievement_id.should == 1
-      achievement_item.completion_date.should == 13333333
+      achievement_item
     end
   end
 
@@ -24,8 +23,8 @@ describe Starcraft2::Profile::AchievementItem do
 
     before do
       @items = [
-        {:achievement_id => 1, :completion_date => 13333333},
-        {:achievement_id => 2, :completion_date => 13333334}
+        {'achievementId' => 1, 'completionDate' => 13333333},
+        {'achievementId' => 2, 'completionDate' => 13333334}
       ]
     end
 
@@ -41,6 +40,12 @@ describe Starcraft2::Profile::AchievementItem do
       a2.class.should == Starcraft2::Profile::AchievementItem
       a2.achievement_id.should == 2
       a2.completion_date.should == 13333334
+    end
+
+    it 'should build items using new' do
+      Starcraft2::Profile::AchievementItem.should_receive(:new).twice
+
+      achievement_items
     end
   end
 end
--- spec/starcraft2/profile/achievements_spec.rb
+++ spec/starcraft2/profile/achievements_spec.rb
@@ -1,63 +1,44 @@
 require 'spec_helper'
 
 describe Starcraft2::Profile::Achievements do
-  let(:achievements) { Starcraft2::Profile::Achievements.new(@options) }
+  describe '.initialize' do
+    let(:achievements) { Starcraft2::Profile::Achievements.new(@options) }
 
-  it 'should store underscored attributes' do
-    @options = {
-      :points => {
-        :total_points => 3420,
-        :category_points => {
-          '4325382' => 0,
-          '4325380' => 80
-        }
-      },
-      :achievements => [
-        {
-          :achievement_id => 91475035553845,
-          :completion_date => 1365790265
-        }
-      ]
-    }
+    before do
+      @options = {
+        'points' => {
+          'totalPoints' => 3420,
+          'categoryPoints' => {
+            '4325382' => 0,
+            '4325380' => 80
+          }
+        },
+        'achievements' => [
+          {
+            'achievementId' => 91475035553845,
+            'completionDate' => 1365790265
+          }
+        ]
+      }
+    end
 
-    achievements.points.class.should == Starcraft2::Profile::Points
-    achievements.points.total_points.should == 3420
-    achievements.points.category_points.should == @options[:points][:category_points]
+    it 'should store attributes as underscored' do
+      achievements.points.class.should == Starcraft2::Profile::Points
+      achievements.points.total_points.should == 3420
+      achievements.points.category_points.should == @options['points']['categoryPoints']
 
-    achievements.achievements.class.should == Array
+      achievements.achievements.class.should == Array
 
-    achievement_item = achievements.achievements.first
-    achievement_item.class.should == Starcraft2::Profile::AchievementItem
-    achievement_item.achievement_id.should == 91475035553845
-    achievement_item.completion_date.should == 1365790265
-  end
+      achievement_item = achievements.achievements.first
+      achievement_item.class.should == Starcraft2::Profile::AchievementItem
+      achievement_item.achievement_id.should == 91475035553845
+      achievement_item.completion_date.should == 1365790265
+    end
 
-  it 'should store camelCased attributes as underscored' do
-    @options = {
-      'points' => {
-        'totalPoints' => 3420,
-        'categoryPoints' => {
-          '4325382' => 0,
-          '4325380' => 80
-        }
-      },
-      'achievements' => [
-        {
-          'achievementId' => 91475035553845,
-          'completionDate' => 1365790265
-        }
-      ]
-    }
+    it 'should use Stracraft2::Utils.load to populate the model' do
+      Starcraft2::Utils.should_receive(:load).with(anything, @options, {:points => Starcraft2::Profile::Points}, {:achievements => Starcraft2::Profile::AchievementItem})
 
-    achievements.points.class.should == Starcraft2::Profile::Points
-    achievements.points.total_points.should == 3420
-    achievements.points.category_points.should == @options['points']['categoryPoints']
-
-    achievements.achievements.class.should == Array
-
-    achievement_item = achievements.achievements.first
-    achievement_item.class.should == Starcraft2::Profile::AchievementItem
-    achievement_item.achievement_id.should == 91475035553845
-    achievement_item.completion_date.should == 1365790265
+      achievements
+    end
   end
 end
