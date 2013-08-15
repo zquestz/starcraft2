9f9cb2d07a8487dd2f2622c05991a9c5190d2f6f
James Fickel <jamesfickel@gmail.com>
2013-08-15 12:26:35 -0700

    not sure why ladders aren't building

--- lib/starcraft2/profile/ladder.rb
+++ lib/starcraft2/profile/ladder.rb
@@ -1,10 +1,15 @@
 module Starcraft2
   class Profile
     class Ladder
-      attr_accessor :ladderName, :ladder_id, :division, :rank, :league, :match_making_queue, :wins, :losses, :showcase
+      attr_accessor :ladder_name, :ladder_id, :division, :rank, :league, :match_making_queue, :wins, :losses, :showcase
 
       def initialize(options = {})
         Utils.load(self, options)
+      end
+
+    def self.build(ladder_data)
+      ladder_data.map do |l|
+        new(l)
       end
     end
   end
