48823c9e09036e328525b49cb6ac34d8887a614f
Josh Ellithorpe <quest@mac.com>
2013-09-26 21:42:17 -0700

    Fix build

--- lib/starcraft2/web_resource.rb
+++ lib/starcraft2/web_resource.rb
@@ -1,27 +1,29 @@
 require 'base64'
 require 'digest/hmac'
+require 'addressable/uri'
 
 class WebResource
   def self.get(url)
     private_key = ENV['STARCRAFT2_API_PRIVATE_KEY']
     public_key = ENV['STARCRAFT2_API_PUBLIC_KEY']
     response = if public_key && private_key
-      HTTParty.get(url, headers: {"Authorization" => "BNET #{public_key}:#{signature(url, private_key)}"})
+      HTTParty.get(url,
+        :headers => { "Authorization" => "BNET #{public_key}:#{signature(url, private_key)}" })
     else
       HTTParty.get(url)
     end
   end
 
   def self.signature(url, private_key)
-    url_path = Addressable::URI.parse(url).path
+    url_path = ::Addressable::URI.parse(url).path
 
     signature = <<-EOF
 GET
-#{Time.now.rfc2822}
+#{Time.now.gmtime.rfc2822.gsub("-0000", "GMT")}
 #{url_path}
 EOF
-    hd = Digest::HMAC.hexdigest(signature.encode('UTF-8'), private_key, Digest::SHA1)
+    hd = Digest::HMAC.hexdigest(signature, private_key.encode('UTF-8'), Digest::SHA1)
 
-    Base64.encode64("#{hd}")
+    Base64.encode64(hd).chomp
   end
 end
