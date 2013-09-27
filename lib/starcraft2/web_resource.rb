ce94f3f4e50b0201638e0383ef2dbd0096b89a0a
James Fickel <jamesfickel@gmail.com>
2013-09-26 20:53:49 -0700

    added some encryption stuff for auth

--- lib/starcraft2/web_resource.rb
+++ lib/starcraft2/web_resource.rb
@@ -1,5 +1,28 @@
+require 'base64'
+require 'hmac-sha1'
+
 class WebResource
   def self.get(url)
-    response = HTTParty.get(url)
+    private_key = ENV['STARCRAFT2_API_PRIVATE_KEY']
+    public_key = ENV['STARCRAFT2_API_PUBLIC_KEY']
+    response = if public_key && private_key
+      HTTParty.get(url, headers: {"Authorization" => "BNET #{public_key}:#{signature(url, private_key)}"})
+    else
+      HTTParty.get(url)
+    end
+  end
+
+  def self.signature(url, private_key)
+    url_path = Addressable::URI.parse(url).path
+
+    signature = <<-EOF
+GET
+#{Time.now.rfc2822}
+#{url_path}
+EOF
+    hmac = HMAC::SHA1.new(private_key)
+    hmac.update(signature.encode('UTF-8'))
+
+    Base64.encode64("#{hmac.digest}")
   end
 end
